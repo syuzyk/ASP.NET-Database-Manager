dc5b1d310eb7a6e9c020de28046fb21caef08c2d
Stephen Yuzyk <stephen.yuzyk@edu.sait.ca>
2021-01-29 14:18:49 -0700

    St

--- Workshop4Group8/Form1.Designer.cs
+++ Workshop4Group8/Form1.Designer.cs
@@ -30,7 +30,6 @@ namespace Workshop4Group8
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -43,23 +42,13 @@ namespace Workshop4Group8
             this.label1.TabIndex = 0;
             this.label1.Text = "A New Beginning\r\n\r\nBy Ricky";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(191, 358);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 17);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Stephen pt2";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -70,7 +59,6 @@ namespace Workshop4Group8
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
     }
 }
 
