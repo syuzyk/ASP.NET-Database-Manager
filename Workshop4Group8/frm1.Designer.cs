b3665b6b09db5d05084fd2ca7df853ea15574c13
dhaval312 <shahdhaval660.ds@gmail.com>
2021-02-06 14:15:53 -0700

    resize thing there is still some problem it's not working with maximizing the screen and also not on main resize method it works on resize end event

--- Workshop4Group8/frm1.Designer.cs
+++ Workshop4Group8/frm1.Designer.cs
@@ -31,52 +31,56 @@ namespace Workshop4Group8
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm1));
             this.Packages = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.product = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Packages
             // 
             this.Packages.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Packages.Location = new System.Drawing.Point(56, 389);
-            this.Packages.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Packages.Location = new System.Drawing.Point(75, 479);
+            this.Packages.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Packages.Name = "Packages";
-            this.Packages.Size = new System.Drawing.Size(273, 87);
+            this.Packages.Size = new System.Drawing.Size(364, 107);
             this.Packages.TabIndex = 1;
             this.Packages.Text = "Manage Packages";
             this.Packages.UseVisualStyleBackColor = true;
             this.Packages.Click += new System.EventHandler(this.Packages_Click);
             // 
-            // button3
+            // product
             // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(481, 389);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(273, 87);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Manage Products";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.product.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.product.Location = new System.Drawing.Point(641, 479);
+            this.product.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.product.Name = "product";
+            this.product.Size = new System.Drawing.Size(364, 107);
+            this.product.TabIndex = 2;
+            this.product.Text = "Manage Products";
+            this.product.UseVisualStyleBackColor = true;
+            this.product.Click += new System.EventHandler(this.button3_Click);
             // 
             // frm1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(814, 523);
-            this.Controls.Add(this.button3);
+            this.ClientSize = new System.Drawing.Size(1085, 644);
+            this.Controls.Add(this.product);
             this.Controls.Add(this.Packages);
             this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frm1";
             this.Text = "Package and Product Manager - Home";
+            this.AutoSizeChanged += new System.EventHandler(this.frm1_Resize);
+            this.Load += new System.EventHandler(this.frm1_Load);
+            this.ResizeEnd += new System.EventHandler(this.frm1_Resize);
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private System.Windows.Forms.Button Packages;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button product;
     }
 }
 
--- Workshop4Group8/frm1.cs
+++ Workshop4Group8/frm1.cs
@@ -10,11 +10,19 @@ using System.Windows.Forms;
 
 namespace Workshop4Group8
 {
+    
     public partial class frm1 : Form
     {
+        //to set the main location and zise of every component
+        public Size orgFrm;
+        public Rectangle orgPackge;
+        public Rectangle orgProduct;
+       
         public frm1()
         {
+
             InitializeComponent();
+            
         }
 
         private void Packages_Click(object sender, EventArgs e)
@@ -27,6 +35,39 @@ namespace Workshop4Group8
         {
             frmPS newform = new frmPS();
             DialogResult result = newform.ShowDialog();
+        }
+
+        private void frm1_Load(object sender, EventArgs e)
+        {
+            orgFrm = Size;
+            orgPackge = new Rectangle(Packages.Location.X, Packages.Location.Y, Packages.Width, Packages.Height);
+            orgProduct = new Rectangle(product.Location.X, product.Location.Y, product.Width, product.Height);
+        }
+        public void resize()
+        {
+            changezise(orgPackge, Packages);
+            changezise(orgProduct, product);
+        }
+        //this is for changing the size and location
+        public void changezise(Rectangle org,Control cntrl)
+        {
+            float xratio = (float)(Size.Width)/(float)(orgFrm.Width);
+            float yratio = (float)(Size.Height)/(float)(orgFrm.Height);
+
+
+            int newx = (int)(org.X * xratio);
+            int newy = (int)(org.Y * yratio);
+            int newwidth = (int)(org.Width * xratio);
+            int newheight = (int)(org.Height * yratio);
+          
+            cntrl.Location = new Point(newx, newy);
+            cntrl.Size = new Size(newwidth, newheight);
+        }
+        
+        //event resize when the main screen is resized every control in it resizes with it
+        private void frm1_Resize(object sender, EventArgs e)
+        {
+         resize();
         }
     }
 }
